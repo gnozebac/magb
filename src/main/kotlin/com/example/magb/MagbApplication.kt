61dd5c1f575cfd88139fbba6234d1928ca9fd8d5
Marco <magbmail@gmail.com>
2023-02-17 22:07:08 -0500

    first commit

---
+++ src/main/kotlin/com/example/magb/MagbApplication.kt
@@ -0,0 +1,11 @@
+package com.example.magb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagbApplication
+
+fun main(args: Array<String>) {
+	runApplication<MagbApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/magb/MagbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.magb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MagbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
